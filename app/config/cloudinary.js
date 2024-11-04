6ea907214bb81744f60dfd4657230832272a1f43
Huy Hào <148667669+HuyHao2512@users.noreply.github.com>
2024-11-04 12:53:41 +0700

    Update cloudinary.js

--- app/config/cloudinary.js
+++ app/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dsfajbqyx",
-  api_key: "822237391191369",
-  api_secret: "k9BAYwaWCs805qVr1pLZGzHQaaU", // Click 'View API Keys' above to copy your API secret
+  cloud_name: "cloud_name",
+  api_key: "api_key",
+  api_secret: "api_secret", // Click 'View API Keys' above to copy your API secret
 });
 
 module.exports = cloudinary;
